86a380f5b1ce839af04a08f1c8f2697184cdf23f
AlexVanderbist <AlexVanderbist@users.noreply.github.com>
2022-05-16 12:13:39 +0000

    Fix styling

--- tests/FlareMiddleware/AddEnvironmentInformationTest.php
+++ tests/FlareMiddleware/AddEnvironmentInformationTest.php
@@ -5,11 +5,11 @@ use Spatie\FlareClient\FlareMiddleware\AddEnvironmentInformation;
 use Spatie\FlareClient\Report;
 
 it('adds the php version to a report', function () {
-   $middleware = new AddEnvironmentInformation();
+    $middleware = new AddEnvironmentInformation();
 
-   $report = (new Report())->useContext(new ConsoleContextProvider());
+    $report = (new Report())->useContext(new ConsoleContextProvider());
 
-   $middleware->handle($report, fn (Report $report) => $report);
+    $middleware->handle($report, fn (Report $report) => $report);
 
-   expect($report->allContext())->toHaveKey('env', ['php_version' => phpversion()]);
+    expect($report->allContext())->toHaveKey('env', ['php_version' => phpversion()]);
 });
