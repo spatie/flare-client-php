3d05d49b3f1081d46cdf4f2db633c96718f95427
Ruben Van Assche <rubenvanassche@gmail.com>
2024-05-03 17:37:26 +0200

    Add support for configureable error levels

--- src/Flare.php
+++ src/Flare.php
@@ -216,9 +216,9 @@ class Flare
         return $this;
     }
 
-    public function registerErrorHandler(): self
+    public function registerErrorHandler(int $errorLevels = E_ALL|E_STRICT): self
     {
-        $this->previousErrorHandler = set_error_handler([$this, 'handleError']);
+        $this->previousErrorHandler = set_error_handler([$this, 'handleError'], $errorLevels);
 
         return $this;
     }
