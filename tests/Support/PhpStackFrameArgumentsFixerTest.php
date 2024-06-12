d6882cda0cb5f7b2739048063d4b4b6e2cdedccf
rubenvanassche <rubenvanassche@users.noreply.github.com>
2024-06-12 14:28:04 +0000

    Fix styling

--- tests/Support/PhpStackFrameArgumentsFixerTest.php
+++ tests/Support/PhpStackFrameArgumentsFixerTest.php
@@ -3,7 +3,7 @@
 use Spatie\FlareClient\Flare;
 use Spatie\FlareClient\Tests\TestClasses\TraceArguments;
 
-it('can enable stack trace arguments on a PHP level', function (){
+it('can enable stack trace arguments on a PHP level', function () {
     ini_set('zend.exception_ignore_args', 1);
 
     $report = Flare::make()
