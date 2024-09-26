b8de82046aa8792af37ecae3f1dd60fe9b9a77b4
rubenvanassche <rubenvanassche@users.noreply.github.com>
2024-09-26 14:18:05 +0000

    Fix styling

--- src/AttributesProviders/UserAttributesProvider.php
+++ src/AttributesProviders/UserAttributesProvider.php
@@ -4,24 +4,20 @@ namespace Spatie\FlareClient\AttributesProviders;
 
 class UserAttributesProvider
 {
-
     public function id(mixed $user): string|int|null
     {
         return null;
     }
-
 
     public function fullName(mixed $user): string|null
     {
         return null;
     }
 
-
     public function email(mixed $user): string|null
     {
         return null;
     }
-
 
     public function attributes(mixed $user): array
     {
