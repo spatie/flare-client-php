7ec1cc20af32a02930567feee90ea0df9aa088fb
Yasser Bal <yasserbal@icloud.com>
2025-08-11 10:15:04 +0200

    ReportSanitizer| Class to ensure a $payload is json encodable

---
+++ src/Support/ReportSanitizer.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Spatie\FlareClient\Support;
+
+use JsonException;
+
+class ReportSanitizer
+{
+    // A suffix for replaced entries ensuring they are recognizable
+    const REPLACED_ENTRY_PREFIX = 'Spatie/Flare';
+
+    /**
+     * @param  array<string,array|string|bool|int|null>  $payload
+     * @return array<string|int,mixed>
+     */
+    public static function sanitizePayload(array $payload): array
+    {
+        try {
+            json_encode($payload, JSON_THROW_ON_ERROR);
+
+            return $payload;
+        } catch (JsonException $e) {
+            return self::replaceNonEncodableEntries($payload);
+        }
+    }
+
+    /**
+     * @param  array<string|int,mixed>  $input
+     * @return array<string|int,mixed>
+     */
+    protected static function replaceNonEncodableEntries(array $input): array
+    {
+        foreach ($input as $key => $value) {
+            try {
+                json_encode($input[$key], JSON_THROW_ON_ERROR);
+            } catch (JsonException $e) {
+                if (is_string($value)) {
+                    $input[$key] = sprintf('[%s: CORRUPT DATA]: %d bytes | ERR: %s', self::REPLACED_ENTRY_PREFIX, strlen($value), $e->getMessage());
+                } elseif (is_array($value)) {
+                    $input[$key] = self::replaceNonEncodableEntries($value);
+                } else {
+                    $input[$key] = sprintf('[%s: NON ENCODABLE]: type %s', self::REPLACED_ENTRY_PREFIX, gettype($value));
+                }
+            }
+        }
+
+        return $input;
+    }
+}
